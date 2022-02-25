7407f0fb7f6ac0bd5334c0ec6cdaf7d16f3a22d7
Lorenzx <webdesign.lorenzo@gmail.com>
2022-02-25 16:26:30 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header flex">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>RnR Restaurant Menu</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
