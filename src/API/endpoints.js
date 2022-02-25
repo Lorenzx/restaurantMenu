d6c9feb58a2814010936f3c3dfc967abc0fbcdb3
Lorenzx <webdesign.lorenzo@gmail.com>
2022-02-25 16:51:34 +0100

    added axios and sample categories api call in hp

---
+++ src/API/endpoints.js
@@ -0,0 +1,3 @@
+// List all meal categories
+export const mealCategoriesEndPoint =
+  "https://www.themealdb.com/api/json/v1/1/categories.php";
--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import { mealCategoriesEndPoint } from "./API/endpoints";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(mealCategoriesEndPoint)
+      .then((res) => setCategories(res.data.categories));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header flex">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>RnR Restaurant Menu</p>
+        <div>
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <div>
+          <p>RnR Restaurant Menu</p>
+
+          <div>
+            {categories.map((item) => (
+              <div>{item.strCategory}</div>
+            ))}
+          </div>
+        </div>
       </header>
     </div>
   );
