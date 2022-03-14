d86275c830705e8e1a2f8db0a491e2839a18477d
Lorenzx <webdesign.lorenzo@gmail.com>
2022-03-14 18:25:23 +0100

    minor fixes

--- src/utils/helpersFuncs.js
+++ src/utils/helpersFuncs.js
@@ -1,3 +1,3 @@
 export const randomNumber = () => {
-  return "-" + Math.floor(Math.random() * (99999 + 1) + 1).toString(); //The maximum is inclusive and the minimum is inclusive
+  return "-" + Math.floor(Math.random() * (99999 + 1) + 1).toString();
 };
